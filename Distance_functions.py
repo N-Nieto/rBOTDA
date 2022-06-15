cb30b825fb50fecb54019f8c601b5a1c27ff76fe
Nicolás Nieto <nnieto@sinc.unl.edu.ar>
2022-06-15 18:05:28 -0300

    Update Distance_functions.py

--- Distance_functions.py
+++ Distance_functions.py
@@ -75,15 +75,23 @@ def distance_to_mean(X,Y):
 
   return dist
 
-def balance_weights(y, w):
+def balance_weights(y, w,relevance = []):
     # Get classes for labels
     classes = np.unique(y)
 
+    if relevance == []:
+      # Uniform relevance for each class
+      relevance = 1/len(classes)*np.ones(classes.shape)
+    elif sum(relevance)==1:
+      raise Exception("Relevance vector needs to sum = 1")
+    elif relevance.shape == classes.shape:
+      raise Exception("Relevance vector needs to have the same shape as the number of classes")
+      
     # Initialize the new weigths
-    w_f = np.zeros(w.shape)
+    w_final = np.zeros(w.shape)
     
     for cl in classes:
-      
+      rel = relevance[classes==cl]
       # total points 
       w_cl = w.copy()
         
@@ -93,78 +101,95 @@ def balance_weights(y, w):
       if sum(w_cl) == 0:
         w_cl =  np.ones((len(w_cl),)) / len(w_cl)
 
-
-      # Change the weight of the points for one class
-      w_cl = w_cl / ( sum(w_cl) * len(classes) )
+      # Normalilize the weights with respect to the relevance of the class and the total mass of the class
+      w_cl = w_cl / ( sum(w_cl) * rel )
       
       # Sum the weight of the particular class to the final weight vector
-      w_f = w_f + w_cl
-    return w_f
+      w_final = w_final + w_cl
+    return w_final
 
-
-def penalized_coupling(Xs, Xt, yt, ys, clf, k=1, metric="euclidean",penalized="distance", ot_method="emd", wrong_cls=True, balanced_target=True, balanced_source=True, reg_e=1, eta = 0.1):
-  """
-    Returns
-    -------
-    G0 :  ((ns x nt) ndarray) – Optimal transportation matrix for the given parameters
-    """
-
-  if Xs.shape[0]!=ys.shape[0]:
-    print("Missmach source samples")
-
-  if Xt.shape[0]!=yt.shape[0]:
-    print("Missmach target samples")
-
-
-  # Source uniform weights
+def deal_with_wrong_classified_point(Xt,yt,clf, clf_retrain, wrong_cls, ot_method):
+  # Delete the wrong classified point from the analysis and
+  if clf_retrain:
+    clf.fit(Xt,yt)
+    Xt, yt = delete_wrong_classified(clf,Xt,yt)
+    clf.fit(Xt,yt)
+    
+  # Start with Source uniform weights
   a = np.ones((len(Xs),)) / len(Xs)
 
-  # Target uniform weights
+  # Start with Target uniform weights
   b =  np.ones((len(Xt),)) / len(Xt)
 
+  # Do not assign mass to missclassified points
   if wrong_cls:
-    # do not assign mass to missclassified points
     Y_pred = clf.predict(Xt)
     b[yt!=Y_pred] = 0
     
+    # If all the datapoins for one classe were missclassified
     if sum(b)==np.NaN:
       # Target uniform weights
       b =  np.ones((len(Xt),)) / len(Xt)
 
+    # Replace mass=0 for Grup Lasso method
     if ot_method == "sinkhorn_gl" or ot_method == "s_gl":
         # Not suport mass==0
         b[yt!=Y_pred] = 1e-10
-    
-  # Calculate the distance of each point to lda decision straight
+
+  return Xt, yt, clf, a, b
+
+def compute_penalization(Xt,clf,b, k,penalized):
+    # Calculate the distance of each point to lda decision straight
   if penalized == "distance" or penalized == "d":
       Q = Q_matix(Xt, clf, k=k)
       
   elif penalized == "proba" or penalized == "p":
       Q = Proba_matix(Xt, clf,k=k)
   else:
-      Warning("Penalized not supported")
+      raise Exception("Penalized not supported")
   
-  # change the point weight proportionaly to its lda hiperplane
+  # change the point weight proportionaly to the computed score
   b = np.dot(b,Q)
- 
-  # Balance target weights
-  if balanced_target:
-    b_final = balance_weights(yt, b)
+
+  return b
+
+def compute_balance_weights(a,b,ys,yt,balanced_source,balanced_target):
+  # Balance target
+  # Chek if empy or False
+  if not(balanced_target):
+    # If empty use uniform relevance for each class
+    if balanced_target==[]:
+      b_final = balance_weights(yt, b, balanced_target)
+    # If False uniform balance
+    else:
+      b_final = b/sum(b)
+  # If not False or Empty check if the first element is int or float
+  elif isinstance(balanced_target[0],(int,float)):
+      b_final = balance_weights(yt, b, balanced_target)
   else:
-    # Uniform normalization
-    b_final = b/sum(b)
+    raise Exception("Balance target not supported")
 
-  # Balance source weights
-  if balanced_source:
-    a_final = balance_weights(ys, a)
+  # Balance source
+  # Chek if empy or False
+  if not(balanced_source):
+    # If empty use uniform relevance for each class
+    if balanced_source==[]:
+      a_final = balance_weights(ys, a, balanced_source)
+    # If False not normalization
+    else:
+      a_final = a
+  # If not False or Empty check if the first element is int or float
+  elif isinstance(balanced_source[0],(int,float)):
+      a_final = balance_weights(ys, a, balanced_source)
   else:
-    # Uniform normalization
-    a_final = a
+    raise Exception("Balance source not supported")
 
-  # Compute cost matrix
-  M = ot.dist(Xs, Xt, metric = metric)
+  return a_final , b_final 
 
-  ##### Compute coupling
+
+def compute_coupling(ot_method, a_final, b_final, M, Xs, Xt, reg_e, eta):
+
+  # Compute coupling
   if ot_method == "emd":
     G0 = ot.lp.emd(a_final, b_final, M)
 
@@ -180,38 +205,79 @@ def penalized_coupling(Xs, Xt, yt, ys, clf, k=1, metric="euclidean",penalized="d
   
   elif ot_method == "emd_laplace" or ot_method == "emd_l":
     G0 = ot.da.emd_laplace(a_final,b_final,Xs, Xt, M, eta=eta)
-
+  else:
+      raise Exception("OT method not supported")
   return G0
 
+def penalized_coupling(Xs, ys, Xt, yt,  clf, k=1, metric="euclidean", penalized="p", ot_method="emd", wrong_cls=True, balanced_target=[], balanced_source=[], clf_retrain=False, reg_e=1, eta = 0.1):
+  """
+    Returns
+    -------
+    G0 :  ((ns x nt) ndarray) – Optimal transportation matrix for the given parameters
+    """
+  # Data sanity check
+  if Xs.shape[0]!=ys.shape[0]:
+    raise Exception("Missmach source samples")
 
-def delete_wrong_classified (lda,X,Y):
-  Y_pred = lda.predict(X)
+  if Xt.shape[0]!=yt.shape[0]:
+    raise Exception("Missmach target samples")
+
+  # Deal with wrong classified point in the target domain
+  Xt, yt, clf, a, b =  deal_with_wrong_classified_point(Xt, yt, clf, clf_retrain, wrong_cls, ot_method)
+
+  # Change the weights of the target points with respect a penalization
+  b = compute_penalization(Xt, clf, b, k, penalized)
+ 
+  # Balance weights
+  a , b = compute_balance_weights(a, b, ys, yt, balanced_source, balanced_target)
+
+  # Compute cost matrix
+  M = ot.dist(Xs, Xt, metric = metric)
+
+  # Compute coupling with different OT methods
+  G0 = compute_coupling(ot_method, a, b, M, Xs, Xt, reg_e, eta)
+
+  if clf_retrain:
+    return G0, clf
+  
+  else:
+    return G0
+
+def delete_wrong_classified(clf,X,Y):
+  # Make a prediction
+  Y_pred = clf.predict(X)
+  # Delete the data missclassified
   X = X[Y==Y_pred]
   Y = Y[Y==Y_pred]
+
   return X, Y
 
 
-def best_k(ot_emd,clf,Xs,Xt,yt,ys,penalized,search=23):
-
+def best_k(ot_emd,Xs, ys, Xt, yt, clf, search=23, metric="euclidean", penalized="p", ot_method="emd", wrong_cls=True, balanced_target=[], balanced_source=[], clf_retrain=False, reg_e=1, eta = 0.1):
+  # Initialization
   acc = 0
-  for k in range(search):
 
+  for k in range(search):
     # Compute the new coupling with the penalized cost matrix
-    G01 = penalized_coupling(Xs,Xt,yt,ys,clf=clf,k=k,penalized=penalized)
+    G01 = penalized_coupling(Xs, ys, Xt, yt,  clf, k=k, metric=metric,penalized=penalized, ot_method=ot_method, wrong_cls=wrong_cls, balanced_target=balanced_target, balanced_source=balanced_source, clf_retrain=clf_retrain, reg_e=reg_e, eta = eta)
 
     # Replace the coupling
     ot_emd.coupling_ = G01
 
-    # transport source samples onto target samples
+    # Transport source samples onto target samples
     T_Xs = ot_emd.transform(Xs=Xs)
-
+    
+    # Compute accuracy
     acc = np.append(acc,clf.score(T_Xs,ys))
-
+  
+  # Delete initialization
   acc = np.delete(acc,0)
+  # Get the max accuracy
   acc_max = np.max(acc)
-  # get the number of nodes where the acc was max
+  # get the k where the acc was max
   acc_max_pos = np.where(acc==acc_max)
   # get the min position
   pos = np.min(acc_max_pos)
+
   return pos
 
